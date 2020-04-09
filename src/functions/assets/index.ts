8a24828bc6409cc518ce37c1b6292bb001a9e089
1amageek <tmy0x3@icloud.com>
2020-04-09 15:55:47 +0900

    Fix Asset

--- src/functions/assets/index.ts
+++ src/functions/assets/index.ts
@@ -5,15 +5,17 @@ const app = express()
 
 app.get('*', async (req, res, next) => {
 	res.set('Cache-Control', 'public, max-age=3600, s-maxage=6000')
-	const bucket = admin.storage().bucket()
 	const path = req.path.slice(1)
-	try {
-		const response = await bucket.file(path).download()
-		if (path.includes('.jpg') || path.includes('jpeg')) res.type('jpg')
-		if (path.includes('.png')) res.type('png')
-		res.status(200).send(response[0])
-	} catch (error) {
-		next(error)
+	if (path.includes('.jpg') || path.includes('.jpeg') || path.includes('.png')) {
+		const bucket = admin.storage().bucket()
+		try {
+			const response = await bucket.file(path).download()
+			if (path.includes('.jpg') || path.includes('.jpeg')) res.type('jpg')
+			if (path.includes('.png')) res.type('png')
+			res.status(200).send(response[0])
+		} catch (error) {
+			next(error)
+		}
 	}
 })
 
