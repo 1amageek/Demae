ab6bd2d60e0a239095f00dc3bbfda8f4d728e9fb
1amageek <tmy0x3@icloud.com>
2020-05-05 02:38:49 +0900

    Fix

--- setup.sample.sh
+++ setup.sample.sh
@@ -1,5 +1,8 @@
 firebase use default
 
 firebase functions:config:set \
-stripe.api_key="sk_test_" \
+stripe.api_key="sk_test_xxxxx" \
+stripe.account="acct_xxxxxx" \
+stripe.account_link_success_url="https://localhost:3000" \
+stripe.account_link_failure_url="https://localhost:3000" \
 firebase functions:config:get
