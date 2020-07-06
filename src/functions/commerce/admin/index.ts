df9ef4c5441ddcf8c3226824853e2d7f3c3e9fec
1amageek <tmy0x3@icloud.com>
2020-07-06 15:28:22 +0900

    Fix

--- src/functions/commerce/admin/index.ts
+++ src/functions/commerce/admin/index.ts
@@ -1,6 +1,7 @@
 import * as admin from "firebase-admin"
 import * as functions from "firebase-functions"
 import { regionFunctions } from "../../helper"
+import User from "../../models/commerce/User"
 
 export const attach = regionFunctions.https.onCall(async (data, context) => {
 	if (!context.auth) {
@@ -15,6 +16,10 @@ export const attach = regionFunctions.https.onCall(async (data, context) => {
 	const providerID = data.providerID
 	if (!providerID) {
 		throw new functions.https.HttpsError("invalid-argument", "This request does not include an providerID.")
+	}
+	const snapshot = await new User(uid).providers.collectionReference.doc(providerID).get()
+	if (!snapshot.exists) {
+		throw new functions.https.HttpsError("invalid-argument", "You do not have the authority of the provider..")
 	}
 	try {
 		await admin.auth().setCustomUserClaims(uid, { admin: providerID })
